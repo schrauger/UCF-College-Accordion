448120c938fd768890b7194ab915309162471d8e
Stephen Schrauger <stephen@schrauger.com>
2019-02-11 09:21:53 -0500

    initial commit

---
+++ includes/acf-pro-fields.php
@@ -0,0 +1,141 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: stephen
+ * Date: 2019-02-01
+ * Time: 1:47 PM
+ */
+class ucf_college_accordion_acf_pro_fields {
+
+    const shortcode = 'ucf_college_accordion';
+    
+    function __construct() {
+        add_action('acf/init', array('ucf_college_accordion_acf_pro_fields','create_fields'));
+    }
+
+    static function create_fields() {
+        if ( function_exists( 'acf_add_local_field_group' ) ) {
+            acf_add_local_field_group(
+                array(
+                    'key'                   => 'group_5c54a4e6e3503',
+                    'title'                 => 'Accordion',
+                    'fields'                => array(
+                        array(
+                            'key'               => 'field_5c54a58f7d56e',
+                            'label'             => 'Accordion start',
+                            'name'              => '',
+                            'type'              => 'accordion',
+                            'instructions'      => '',
+                            'required'          => 0,
+                            'conditional_logic' => 0,
+                            'wrapper'           => array(
+                                'width' => '',
+                                'class' => '',
+                                'id'    => '',
+                            ),
+                            'open'              => 1,
+                            'multi_expand'      => 0,
+                            'endpoint'          => 0,
+                        ),
+                        array(
+                            'key'               => 'field_5c54a5ff7d570',
+                            'label'             => 'Accordion item',
+                            'name'              => 'accordion_item',
+                            'type'              => 'repeater',
+                            'instructions'      => '',
+                            'required'          => 0,
+                            'conditional_logic' => 0,
+                            'wrapper'           => array(
+                                'width' => '',
+                                'class' => '',
+                                'id'    => '',
+                            ),
+                            'collapsed'         => '',
+                            'min'               => 1,
+                            'max'               => 0,
+                            'layout'            => 'table',
+                            'button_label'      => '',
+                            'sub_fields'        => array(
+                                array(
+                                    'key'               => 'field_5c54a6147d571',
+                                    'label'             => 'Header',
+                                    'name'              => 'header',
+                                    'type'              => 'text',
+                                    'instructions'      => '',
+                                    'required'          => 0,
+                                    'conditional_logic' => 0,
+                                    'wrapper'           => array(
+                                        'width' => '',
+                                        'class' => '',
+                                        'id'    => '',
+                                    ),
+                                    'default_value'     => '',
+                                    'placeholder'       => '',
+                                    'prepend'           => '',
+                                    'append'            => '',
+                                    'maxlength'         => '',
+                                ),
+                                array(
+                                    'key'               => 'field_5c54a6187d572',
+                                    'label'             => 'Text',
+                                    'name'              => 'text',
+                                    'type'              => 'text',
+                                    'instructions'      => '',
+                                    'required'          => 0,
+                                    'conditional_logic' => 0,
+                                    'wrapper'           => array(
+                                        'width' => '',
+                                        'class' => '',
+                                        'id'    => '',
+                                    ),
+                                    'default_value'     => '',
+                                    'placeholder'       => '',
+                                    'prepend'           => '',
+                                    'append'            => '',
+                                    'maxlength'         => '',
+                                ),
+                            ),
+                        ),
+                        array(
+                            'key'               => 'field_5c54a6217d573',
+                            'label'             => 'Accordion end',
+                            'name'              => '',
+                            'type'              => 'accordion',
+                            'instructions'      => '',
+                            'required'          => 0,
+                            'conditional_logic' => 0,
+                            'wrapper'           => array(
+                                'width' => '',
+                                'class' => '',
+                                'id'    => '',
+                            ),
+                            'open'              => 0,
+                            'multi_expand'      => 0,
+                            'endpoint'          => 1,
+                        ),
+                    ),
+                    'location'              => array(
+                        array(
+                            array(
+                                'param'    => 'post_taxonomy',
+                                'operator' => '==',
+                                'value'    => 'ucf_shortcode_category:' . self::shortcode,
+                            ),
+                        ),
+                    ),
+                    'menu_order'            => 0,
+                    'position'              => 'normal',
+                    'style'                 => 'default',
+                    'label_placement'       => 'top',
+                    'instruction_placement' => 'label',
+                    'hide_on_screen'        => '',
+                    'active'                => 1,
+                    'description'           => '',
+                ) );
+
+        }
+    }
+}
+
+new ucf_college_accordion_acf_pro_fields();
---
+++ includes/ucf-college-accordion-shortcode.php
@@ -0,0 +1,92 @@
+<?php
+
+class ucf_college_accordion_shortcode {
+    const shortcode_slug = 'ucf_college_accordion'; // the shortcode text entered by the user (inside square brackets)
+    const shortcode_name = 'Accordion';
+    const shortcode_description = 'Collapsible sections with headers';
+    //const get_param_group = 'people_group'; // group or category person is in
+
+    function __construct() {
+        add_action( 'init', array( $this, 'add_shortcode' ) );
+        add_filter( 'query_vars', array($this, 'add_query_vars_filter' )); // tell wordpress about new url parameters
+    }
+
+    /**
+     * Adds the shortcode to wordpress' index of shortcodes
+     */
+    function add_shortcode() {
+        if ( ! ( shortcode_exists( self::shortcode_slug ) ) ) {
+            add_shortcode( self::shortcode_slug, array($this, 'replacement' ));
+        }
+    }
+
+    /**
+     * Tells wordpress to listen for the 'people_group' parameter in the url. Used to filter down to specific profiles.
+     * @param $vars
+     *
+     * @return array
+     */
+    function add_query_vars_filter($vars){
+        //$vars[] = self::get_param_group;
+        return $vars;
+    }
+
+    /**
+     * Returns the replacement html that WordPress uses in place of the shortcode
+     *
+     * @return string
+     */
+    function replacement(  ){
+        $replacement_data = ''; //string of html to return
+        if (have_rows('accordion_item')){
+            $replacement_data .= "<div class='accordion'>";
+
+            while (have_rows('accordion_item')){
+                the_row();
+                $header = get_sub_field('header');
+                $data = get_sub_field('text');
+                $replacement_data .= "
+                    <div class='accordion-item'>
+                        <div class='accordion-header'>{$header}</div>
+                        <div class='accordion-data'>{$data}</div>
+                    </div>            
+                ";
+            }
+
+            $replacement_data .= "</div>";
+        }
+
+        return $replacement_data;
+    }
+
+    /**
+     * Only run this on plugin activation, as it's stored in the database
+     */
+    static function insert_shortcode_term(){
+        $taxonomy = new ucf_college_shortcode_taxonomy;
+        $taxonomy->create_taxonomy();
+        wp_insert_term(
+            self::shortcode_name,
+            ucf_college_shortcode_taxonomy::taxonomy_slug,
+            array(
+                'description' => self::shortcode_description,
+                'slug' => self::shortcode_slug
+            )
+        );
+    }
+
+    /**
+     * Run when plugin is disabled and/or uninstalled. This removes the shortcode from the list of shortcodes in the taxonomy.
+     */
+    static function delete_shortcode_term(){
+        $taxonomy = new ucf_college_shortcode_taxonomy;
+        $taxonomy->create_taxonomy();
+        wp_delete_term(get_term_by('slug', self::shortcode_slug)->term_id, ucf_college_shortcode_taxonomy::taxonomy_slug);
+    }
+
+
+
+
+}
+
+new ucf_college_accordion_shortcode();
