95b55bfda431d4d50d7a36a7fc4cb8e1a8ea9e14
Stephen Schrauger <stephen@schrauger.com>
2019-02-15 15:30:44 -0500

    start closed

--- includes/plugin.js
+++ includes/plugin.js
@@ -5,7 +5,8 @@
 jQuery( document ).ready(function($) {
 
 $('.accordion-container').accordion({
-    collapsible: false, // do not allow all to be closed (which automatically causes the first one to be expanded)
+    active: false, // start with all collapsed
+    collapsible: true, // allow all to be closed
     heightStyle: "content", // variable height based on inner content
 });
 
